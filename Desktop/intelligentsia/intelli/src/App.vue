4d57a22d740d0837b9c7dda266bf004d5cd5bb47
cyanosis <69207955+kruzni@users.noreply.github.com>
2023-06-13 03:29:28 +0200

    commit operation

---
+++ Desktop/intelligentsia/intelli/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="gore">
+    <div class="upthere">
+      <a href="" class="materijali">MATERIJALI</a>
+      <a href="" class="kontakt">KONTAKT</a>
+      <a href="" class="o-nama">O NAMA</a>
+      <a href="" class="naslov">INTELLIGENTSIA</a>
+      <a href="" class="donacija">DONACIJA</a>
+      <a href="" class="prijava">PRIJAVA</a>
+      <a href="" class="registracija">REGISTRACIJA</a>
+    </div>
+  </section>
+  <div class="prikaz">
+    <h1 class="platforma">PLATFORMA ZA UČENJE</h1>
+    <h3 class="buduc">POSTANITE DIO PLATFORME KOJA BRINE O VAŠOJ BUDUĆNOSTI</h3>
+  </div>
+  <div></div>
+  <div class="slike">
+    <a href="https://www.w3schools.com/html/" class="html"
+      ><img :src="require('@/assets/html.png')" alt="Photo"
+    /></a>
+    <a href="https://www.w3schools.com/html/" class="css"
+      ><img :src="require('@/assets/css.png')" alt="Photo"
+    /></a>
+    <a href="https://www.w3schools.com/html/" class="js"
+      ><img :src="require('@/assets/js.png')" alt="Photo"
+    /></a>
+    <a href="https://www.w3schools.com/html/" class="py"
+      ><img :src="require('@/assets/python.png')" alt="Photo"
+    /></a>
+    <a href="https://www.w3schools.com/html/" class="cpp"
+      ><img :src="require('@/assets/cpp.png')" alt="Photo"
+    /></a>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
